4b951b4db8454901c92bb68116f4697683034ba3
Erik Vanhainen <erikwanhainen@gmail.com>
2021-04-22 22:05:32 +0200

    val in plot

--- scp_script.py
+++ scp_script.py
@@ -1,7 +1,7 @@
 import os
-for i in range(1, 110, 5):
-    command1 = f'scp -r -v -o "IdentitiesOnly=yes" -i ~/.ssh/id_rsa_personal -P 56027 da150x_14@130.237.37.22:~/kex-gan/src/checkpoints/w_128/ckpt-{i}.data-00000-of-00001 src/checkpoints/w_128'
-    command2 = f'scp -r -v -o "IdentitiesOnly=yes" -i ~/.ssh/id_rsa_personal -P 56027 da150x_14@130.237.37.22:~/kex-gan/src/checkpoints/w_128/ckpt-{i}.index src/checkpoints/w_128'
+for i in range(1, 63, 2):
+    command1 = f'scp -r -v -o "IdentitiesOnly=yes" -i ~/.ssh/id_rsa_personal -P 56027 da150x_14@130.237.37.22:~/kex-gan/src/checkpoints/w_128/ckpt-{i}.data-00000-of-00001 src/checkpoints/w_128_v2/w_128'
+    command2 = f'scp -r -v -o "IdentitiesOnly=yes" -i ~/.ssh/id_rsa_personal -P 56027 da150x_14@130.237.37.22:~/kex-gan/src/checkpoints/w_128/ckpt-{i}.index src/checkpoints/w_128_v2/w_128'
     print('#################################')
     print(i)
     os.system(command1)
