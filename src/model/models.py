437d7fed143d7b1f1f747b4dd7b99610f4c3c4b8
Johan Adamsson <jada@kth.se>
2021-04-26 12:00:51 +0200

    refactor models to sep. file

--- src/model/models.py
+++ src/model/models.py
@@ -1,24 +1,116 @@
+"""
+    This files defines the 
+
+"""
+
+
+
 import tensorflow as tf
+import numpy as np
+from tensorflow.keras import layers
+from tensorflow.keras import activations 
 
-def Deconv(inputs, f_dim_in, dim, net, batch_size, f_dim_out = None, stride = 2 ):
-	if f_dim_out is None: 
-		f_dim_out = f_dim_in/2 
-	return tl.layers.DeConv3dLayer(inputs,
-                shape = [4, 4, 4, f_dim_out, f_dim_in],
-                output_shape = [batch_size, dim, dim, dim, f_dim_out],
-                strides=[1, stride, stride, stride, 1],
-                W_init = tf.random_normal_initializer(stddev=0.02),
-                act=tf.identity, name='g/net_' + net + '/deconv')
-
-def Conv3D(inputs, f_dim_out, net, f_dim_in = None, batch_norm = False, is_train = True):
-	if f_dim_in is None: 
-		f_dim_in = f_dim_out/2
-	layer = tl.layers.Conv3dLayer(inputs, 
-                shape=[4, 4, 4, f_dim_in, f_dim_out],
-                W_init = tf.random_normal_initializer(stddev=0.02),
-                strides=[1, 2, 2, 2, 1], name= 'd/net_' + net + '/conv')
+from src.model.utils import load_params
 
 
-def generator(inputs, batch_size):
-    output_size, half, forth, eighth, sixteenth = 128, 64, 32, 16, 8
-    gf_dim = 256 # Dimension of gen filters in first conv laye
+
+params = load_params()
+
+RESOLUTION = params['RESOLUTION']
+NOISE_DIM = params['NOISE_DIM']
+
+
+def generator_model():
+    """
+      bruh
+    """
+    filters_initial = 256
+    kernel_size = (4, 4, 4)
+    half, fourth, eighth, sixteenth, threetwo = int(RESOLUTION / 2), int(
+        RESOLUTION / 4), int(RESOLUTION / 8), int(RESOLUTION / 16), int(RESOLUTION / 32)
+
+    model = tf.keras.Sequential()
+
+    model.add(layers.Dense(threetwo*threetwo*threetwo*filters_initial,
+                           use_bias=False, input_shape=(NOISE_DIM, )))  # Latent vector z 200 dimensional
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
+
+    model.add(layers.Reshape((threetwo, threetwo, threetwo, filters_initial)))
+    assert model.output_shape == (
+        None, threetwo, threetwo, threetwo, filters_initial)
+
+    model.add(layers.Conv3DTranspose(filters_initial, kernel_size,
+                                     strides=(2, 2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (
+        None, sixteenth, sixteenth, sixteenth, filters_initial)
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
+
+    model.add(layers.Conv3DTranspose(filters_initial / 2, kernel_size,
+                                     strides=(2, 2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (
+        None, eighth, eighth, eighth, filters_initial/2)
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
+
+    model.add(layers.Conv3DTranspose(filters_initial / 4, kernel_size,
+                                     strides=(2, 2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (
+        None, fourth, fourth, fourth, filters_initial/4)
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
+
+    model.add(layers.Conv3DTranspose(filters_initial / 8, kernel_size,
+                                     strides=(2, 2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, half, half, half, filters_initial/8)
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
+
+    model.add(layers.Conv3DTranspose(1, kernel_size, strides=(
+        2, 2, 2), padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, RESOLUTION, RESOLUTION, RESOLUTION, 1)
+
+    return model
+
+def discriminator_model():
+    """
+      Change structure to more layers later (4-5 convolutional layers seems common)
+
+
+      IF WE USE WASSERSTEIN LOSS WITH GRADIENT PENALTY WE NEED TO CHANGE BATCHNORMALIZATION
+      TO e.g. LAYER NORMALIZATION
+    """
+    filters_initial = 256
+    kernel_size = (4, 4, 4)
+
+    model = tf.keras.Sequential()
+    model.add(layers.Conv3D(filters_initial / 8, kernel_size, strides=(2, 2, 2), padding='same',
+                            input_shape=[RESOLUTION, RESOLUTION, RESOLUTION, 1]))
+    # model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv3D(filters_initial / 4, kernel_size,
+                            strides=(2, 2, 2), padding='same'))
+    # model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv3D(filters_initial/2, kernel_size,
+                            strides=(2, 2, 2), padding='same'))
+    # model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv3D(filters_initial, kernel_size,
+                            strides=(2, 2, 2), padding='same'))
+    # model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv3D(1, kernel_size, strides=(1, 1, 1), padding='same'))
+
+    # model.add(layers.Activation(activations.sigmoid))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1, activation='linear'))
+
+    return model
+
