c737dce7d2390884ee3b14ed435f693ae0e9698b
Johan Adamsson <jada@kth.se>
2021-04-28 16:29:04 +0200

    nice

--- src/plot/plot.py
+++ src/plot/plot.py
@@ -40,25 +40,62 @@ checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
 
 
 ds_files = tf.data.Dataset.list_files(
-    f'src/data/{RESOLUTION}/*.npy', shuffle=True)
+    f'../data/{RESOLUTION}/*.npy', shuffle=True)
 dataset = ds_files.map(process_path)
 train_ds = dataset.shuffle(BUFFER_SIZE).batch(
     BATCH_SIZE, drop_remainder=True)  # drop if the data is not evenly split
 
 
 ds_files_val = tf.data.Dataset.list_files(
-    f'src/data/{RESOLUTION}_val/*.npy', shuffle=True)
+    f'../data/{RESOLUTION}_val/*.npy', shuffle=True)
 dataset_val = ds_files_val.map(process_path)
 val_ds = dataset.shuffle(BUFFER_SIZE).batch(
     BATCH_SIZE, drop_remainder=True)  # drop if the data is not evenly split
 # Create distributed dataset depending on strategy
-dist_ds = strategy.experimental_distribute_dataset(train_ds)
 
 
 if RESTORE:
     checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir)).expect_partial()
     print('Restoring last model')
 
+
+
+def loss_plot(tds, vds, epochs):
+    big_boy_t = [] 
+    big_boy_v = []
+    count = 0
+    for e in epochs:
+        t_loss = []
+        v_loss = []
+        checkpoint.restore(os.path.join(checkpoint_dir, f'ckpt-{e}'))
+        for batch in tds:
+            loss = disc_loss_batch(generator, discriminator, batch).numpy()
+            print(type(loss))
+            t_loss.append(loss)
+            count += 1
+            if count == 7:
+                print('BREAKING')
+                count = 0
+                break
+
+
+        for batch in vds:
+            loss = disc_loss_batch(generator, discriminator, batch).numpy()
+            v_loss.append(loss)
+
+        big_boy_t.append(sum(t_loss)/len(t_loss))
+        big_boy_v.append(sum(v_loss)/len(v_loss))
+
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+    ax.plot(epochs, big_boy_t, label='Training')
+    ax.plot(epochs, big_boy_v, label='Validation')
+    ax.set_ylabel('negative disc loss')
+    ax.set_xlabel('epoch')
+    ax.legend()
+    plt.savefig('nice.png')
+
+loss_plot(train_ds, val_ds, range(1,10))
+
 #animated_plot(generator=generator, amount=50, save=False, cutoff=0.5)
 # plot_images(generator=generator, cutoff=0.5)
-test_plot(generator, 0.5)
+# test_plot(generator, 0.5)
