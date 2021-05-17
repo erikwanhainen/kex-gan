4d04303add308aa7cd6cdc56ce827851ac057469
Johan Adamsson <jada@kth.se>
2021-05-17 11:37:00 +0200

    visualize filters

--- src/plot/plot.py
+++ src/plot/plot.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 import os 
+import time
+
+from scipy.ndimage import zoom
 
 from tensorflow.keras import layers
 from tensorflow.keras import activations
@@ -39,18 +42,18 @@ checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
                                  discriminator=discriminator)
 
 
-ds_files = tf.data.Dataset.list_files(
-    f'../data/{RESOLUTION}/*.npy', shuffle=True)
-dataset = ds_files.map(process_path)
-train_ds = dataset.shuffle(BUFFER_SIZE).batch(
-    BATCH_SIZE, drop_remainder=True)  # drop if the data is not evenly split
-
-
-ds_files_val = tf.data.Dataset.list_files(
-    f'../data/{RESOLUTION}_val/*.npy', shuffle=True)
-dataset_val = ds_files_val.map(process_path)
-val_ds = dataset.shuffle(BUFFER_SIZE).batch(
-    BATCH_SIZE, drop_remainder=True)  # drop if the data is not evenly split
+#ds_files = tf.data.Dataset.list_files(
+#    f'../data/{RESOLUTION}/*.npy', shuffle=True)
+#dataset = ds_files.map(process_path)
+#train_ds = dataset.shuffle(BUFFER_SIZE).batch(
+#    BATCH_SIZE, drop_remainder=True)  # drop if the data is not evenly split
+#
+#
+#ds_files_val = tf.data.Dataset.list_files(
+#    f'../data/{RESOLUTION}_val/*.npy', shuffle=True)
+#dataset_val = ds_files_val.map(process_path)
+#val_ds = dataset.shuffle(BUFFER_SIZE).batch(
+#    BATCH_SIZE, drop_remainder=True)  # drop if the data is not evenly split
 # Create distributed dataset depending on strategy
 
 
@@ -94,8 +97,78 @@ def loss_plot(tds, vds, epochs):
     ax.legend()
     plt.savefig('nice.png')
 
-loss_plot(train_ds, val_ds, range(1,10))
 
+def test_ploty(v, cutoff): 
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlim3d(0, 4)
+    ax.set_ylim3d(0, 4)
+    ax.set_zlim3d(0, 4)
+
+    axis = 2
+    plane = 1
+    plot_plane = True
+    if plot_plane:
+        for i in range(0,4):
+            if i == plane:
+                continue
+            elif axis == 0:
+                v[i, :, :] = 0
+            elif axis == 1:
+                v[:, i, :] = 0
+            else:
+                v[:, :, i] = 0
+
+    cmap = plt.get_cmap('binary')(v)
+
+    ax.voxels(v, facecolors=cmap)
+    
+    plt.show()
+
+
+x = None
+layer = discriminator.layers[0]
+print(layer.name)
+print(layer.get_weights()[0].shape)
+# (filter_width, filter_height, filter_depth, input_channels, output_channels)
+n_filters = layer.get_weights()[0].shape[-1]
+n_filters = min(n_filters, 4)
+#for i in range(n_filters):
+#    weight = layer.get_weights()[0][:, :, :, :, i]
+#    for j in range(weight.shape[-1]):
+#        w = weight[:, :, :, j]
+#        print(w.max())
+#        test_ploty(w, 0)
+#        time.sleep(0.1)
+
+
+# plot first few filters
+filters = layer.get_weights()[0]
+# n_filters = outgoing channels
+outgoing_channels = n_filters
+ix = 1
+for i in range(n_filters):
+    # get the filter
+    f = filters[:, :, :, :, i]
+    # plot each channel separately
+    # Range of incoming channels
+    incoming_channels = f.shape[-1]
+    incoming_channels= min(incoming_channels, 5)
+    for j in range(incoming_channels):
+        # Range of Depth of the kernel .i.e. 3
+        Depth = 4
+        for k in range(Depth):
+            # specify subplot and turn of axis
+            ax = plt.subplot((outgoing_channels*4), incoming_channels, ix)
+            ax.set_xticks([])
+            ax.set_yticks([])
+            # plot filter channel in grayscale
+            plt.imshow(f[k, :, :,j], cmap='gray')
+            ix += 1
+# show the figure
+plt.show()
+
+#loss_plot(train_ds, val_ds, range(1,10))
 #animated_plot(generator=generator, amount=50, save=False, cutoff=0.5)
-# plot_images(generator=generator, cutoff=0.5)
-# test_plot(generator, 0.5)
+#plot_images(generator=generator, cutoff=0.5)
+#test_plot(generator, 0.5)
